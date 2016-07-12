a4e22c17168b2f3f14168364f161f8af2b1df420
Godson Ukpere <godson.ukpere@gmail.com>
2016-07-12 02:35:24 +0100

    feature(Logger): Add Logger to show server responses in console

--- Logger.go
+++ Logger.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "net/http"
   "log"
+  "time"
 )
 
 func LogMessage(w http.ResponseWriter, message string) {
@@ -12,4 +13,20 @@ func LogMessage(w http.ResponseWriter, message string) {
 
 func LogError(err error) {
   log.Fatal(err)
+}
+
+func Logger(inner http.Handler, name string) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+
+        inner.ServeHTTP(w, r)
+
+        log.Printf(
+            "%s\t%s\t%s\t%s",
+            r.Method,
+            r.RequestURI,
+            name,
+            time.Since(start),
+        )
+    })
 }
